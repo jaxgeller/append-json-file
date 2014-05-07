eabf7c497146ee99a6d7485c1df7d92b57d3beb4
jacksongeller <jacksongeller@gmail.com>
2014-05-07 16:34:14 -0400

    done.

---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require('fs-extra');
+var _ = require('lodash');
+
+function append(prop, file, cb) {
+  
+  fs.readJson(file, function(err, data) {
+    
+    if (err && err.code === 'ENOENT') {
+      fs.outputJson(file, prop, function(err) {
+        return cb()
+      })
+    }
+    
+    else {
+      var merged = _.merge(data, prop);
+      fs.outputJson(file, merged, function(err) {
+        return cb()
+      })
+    }
+
+  })
+
+}
+
+
+module.exports = append;
