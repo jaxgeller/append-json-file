06692f386089b9e8b351337df14b70cf988e0bb0
jacksongeller <jacksongeller@gmail.com>
2014-05-07 17:47:55 -0400

    update module

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ var _ = require('lodash');
 function append(prop, file, cb) {
   
   fs.readJson(file, function(err, data) {
-    
+
     if (err && err.code === 'ENOENT') {
       fs.outputJson(file, prop, function(err) {
         return cb()
       })
     }
-    
+    else if (!data) {
+      fs.outputJson(file, prop, function(err) {
+        return cb()
+      })
+    }
     else {
       var merged = _.merge(data, prop);
       fs.outputJson(file, merged, function(err) {
